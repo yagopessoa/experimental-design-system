f45d7965587429c089907c42461e99199acfe381
Yago <yagorp15@gmail.com>
2020-09-20 20:28:19 -0300

    refactor(setup): folders re-structured

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -25,7 +25,7 @@ export interface ButtonProps {
 }
 
 /**
- * Primary UI component for user interaction
+ * Primary UI component for user interaction o QUE?
  */
 export const Button: React.FC<ButtonProps> = ({
   primary = false,
@@ -34,11 +34,15 @@ export const Button: React.FC<ButtonProps> = ({
   label,
   ...props
 }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={['storybook-button', `storybook-button--${size}`, mode].join(
+        ' '
+      )}
       style={{ backgroundColor }}
       {...props}
     >
@@ -46,3 +50,5 @@ export const Button: React.FC<ButtonProps> = ({
     </button>
   );
 };
+
+export default Button;
